c3e0005b53f085852dc2cd5eaf1a6bfa6491cbfc
AbdulConsole <hzwedt@gmail.com>
2024-01-20 15:04:00 +0100

    documentting with comments

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
 func main() {
 	fmt.Println("Hello World!")
 
+	// Example fetching from first package
 	getTwo := learn.Two()
 	fmt.Println(getTwo)
 
+	// Example fetching from second package
 	getThree := learn.Three()
 	fmt.Println(getThree)
 }
